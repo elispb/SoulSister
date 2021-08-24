273df402a5503a95e318a9f93f962f7e42068004
Elis Brown <elis.p.brown@gmail.com>
2021-08-24 17:09:59 +0100

    add id

--- SoulSister/Models/Recipe.cs
+++ SoulSister/Models/Recipe.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace SoulSister.Models {
     public class Recipe {
+
+        public int ID { get; set; }
+
         public string Name { get; set; }
 
         public string Duration { get; set; }
