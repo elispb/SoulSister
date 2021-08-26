9fdc246201095f6a73905712d680a82523a4f333
Elis Brown <elis.p.brown@gmail.com>
2021-08-26 23:52:16 +0100

    try to publish site to azure web service

--- SoulSisterSite/Startup.cs
+++ SoulSisterSite/Startup.cs
@@ -28,7 +28,7 @@ namespace SoulSisterSite {
                 app.UseDeveloperExceptionPage();
             }
             else {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/deployment center/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
